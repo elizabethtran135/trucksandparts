e50c091f634e4cc911a826eb54f485148b274663
Elizabeth Tran <elizabethtran135@gmail.com>
2020-04-05 15:39:30 -0400

    centered container and removed >> in READ MORE blog button

--- index.php
+++ index.php
@@ -7,14 +7,14 @@
 </div>
   <div class="container">
     <div class="row p-4">
-      <main class="col-md-9">
+      <main class="col-sm-12 col-md-9">
        <?php if(have_posts()){
           while(have_posts()){
             the_post(); ?>
               <?php the_post_thumbnail('full'); ?>
               <h1 class="font-weight-bold"><?php the_title(); ?></h1>
               <h3><?php the_excerpt();?></h3>
-              <a class="btn btn-primary btn-sm" href="<?php the_permalink(); ?>">Read More >> </a>
+              <a class="btn btn-primary btn-sm" href="<?php the_permalink(); ?>">Read More</a>
             <?php } //ends while loop
           }//ends if statement
 
