01c7ec1d204dcedc0ca8c1a50535b3d1e1cfeb61
Elizabeth Tran <elizabethtran135@gmail.com>
2020-04-23 07:40:51 -0400

    spacing on about sides

--- page-right-sidebar.php
+++ page-right-sidebar.php
@@ -12,7 +12,7 @@ get_header(); ?>
 </div>
   <div class="container">
     <div class="row">
-      <main class="col-md-9">
+      <main class="col-md-9 px-4">
         <?php
           if(have_posts()){
             while(have_posts()){
